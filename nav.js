9bf33f274e13b07b488a56781060c924aa5770c0
Adie Williams <hello@adie.codes>
2016-11-16 06:33:22 +0000

    fix: Responsive nav firing multiple times at small viewport
    Although now it only fires once, if you resize again the .open-menu
     icon isn't shown

--- nav.js
+++ nav.js
@@ -2,9 +2,24 @@ var viewWidth = $(window).width();
 
 // code stolen from [Stack Overflow](http://stackoverflow.com/questions/2854407/javascript-jquery-window-resize-how-to-fire-after-the-resize-is-completed)
 
+var waitForFinalEvent = (function () {
+  var timers = {};
+  return function (callback, ms, uniqueId) {
+    if (!uniqueId) {
+      uniqueId = "Don't call this twice without a uniqueId";
+    }
+    if (timers[uniqueId]) {
+      clearTimeout (timers[uniqueId]);
+    }
+    timers[uniqueId] = setTimeout(callback, ms);
+  };
+})();
+
 $(window).resize(function(){
-  viewWidth = $(window).width();
-  responsiveMenu(viewWidth);
+  waitForFinalEvent(function(){
+    viewWidth = $(window).width();
+    responsiveMenu(viewWidth);
+  }, 500, "some unique string")
 });
 
 function responsiveMenu(viewWidth){
