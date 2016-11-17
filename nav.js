64f7b6fdebb8f6f45d84c040f9d618e4f2f373fa
Adie Williams <hello@adie.codes>
2016-11-17 15:06:08 +0000

    feat: Add transition to hamburger menu

--- nav.js
+++ nav.js
@@ -4,29 +4,30 @@
   var viewportWidth = $(window).innerWidth();
 
   function hamburgerMenu(vw){
+    // add hamburger menu content dynamically, to reduce impact on screen readers
+    var hamburgerMenuContent = '<div class="nav__toggle hide">'+
+      '<button class="open-menu">&#9776;</button>'+
+      '<button class="close-menu hide">&#735;</button>'+
+      '</div><!--.nav__toggle-->';
+    $('.nav__list').before(hamburgerMenuContent);
     if(vw < 381){
-      // add icons if viewport less equal to or less than 380px
-      var hamburgerMenuContent = '<div class="nav__toggle">'+
-        '<button class="open-menu">&#9776;</button>'+
-        '<button class="close-menu hide">&#735;</button>'+
-        '</div><!--.nav__toggle-->';
-      $('.nav__list').before(hamburgerMenuContent);
+      $('.nav__toggle').removeClass('hide');
       $('.nav__list').addClass('hide');
       // display navigation link and cross and hide hamburger icon when hamburger is clicked
       $('.open-menu').click(function(){
-        $('.nav__list').removeClass('hide');
+        $('.nav__list').removeClass('hide').addClass('show');
         $('button.open-menu').addClass('hide');
         $('button.close-menu').removeClass('hide');
       });
       // display hamburger icon and hide navigation links and cross when cross is clicked
       $('.close-menu').click(function(){
-        $('.nav__list').addClass('hide');
+        $('.nav__list').removeClass('show').addClass('hide');
         $('button.open-menu').removeClass('hide');
         $('button.close-menu').addClass('hide');
       })
     } else{
-      // remove hamburger and display nav if viewport is greater than 380px
-      $('.nav__toggle').hide();
+      // hide hamburger content and display nav if viewport is greater than 380px
+      $('.nav__toggle').addClass('hide');
       $('.nav__list').removeClass('hide');
     }
   };
